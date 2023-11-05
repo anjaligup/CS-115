a0b4a8968eebbaa0c9f2b087e9cbd437f99f19dc
Anjali Gupta <134241823+anjaligup@users.noreply.github.com>
2023-11-05 09:43:50 -0500

    Update knapsack.py

--- knapsack.py
+++ knapsack.py
@@ -5,4 +5,10 @@ def knapsack(capacity, items):
   '''
   if len(items) or capacity<=0:
     return 0 
-  elif items[0][0] > 
+  elif items[0][0] > capacity:
+    return knapsack(capacity, items[1:])
+  else:
+    use_it = items[0][1] + knapsack(capacity-items[0][1], items[1:]) #use the weight of that item into consideration
+    lose_it = knapsack(capacity, items[1;]) #lose the weight of that item into consideration 
+    maximize_it = max(use_it, lose_it)
+    return maximize_it #return the max value 
