8f21df2990c80d38ac757a176c5d14731980e317
Anjali Gupta <134241823+anjaligup@users.noreply.github.com>
2023-11-05 09:40:16 -0500

    Create knapsack.py

---
+++ knapsack.py
@@ -0,0 +1,8 @@
+def knapsack(capacity, items):
+  '''
+  this function returns the max total value of items you can bring in your knapack 
+  given an item list which contains weight and value of each object
+  '''
+  if len(items) or capacity<=0:
+    return 0 
+  elif items[0][0] > 
