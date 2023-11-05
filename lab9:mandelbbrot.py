27a653dd85a514297948e39c27930380528796a5
Anjali Gupta <134241823+anjaligup@users.noreply.github.com>
2023-11-05 12:06:30 -0500

    Create lab9:mandelbbrot.py

---
+++ lab9:mandelbbrot.py
@@ -0,0 +1,46 @@
+# mandelbrot.py
+# Lab 9
+#
+# Name: Anjali Gupta
+#I pledge my honor that I have abided by the Stevens Honor System - AG
+
+# keep this import line...
+from cs5png import PNGImage
+
+# start your Lab 9 functions here:
+def mult( c, n ):
+    """ mult uses only a loop and addition
+        to multiply c by the integer n
+    """
+    result = 0
+    for x in range( n ):
+        # update the value of result here in the loop
+        result = result + c
+    return result
+
+
+def update( c, n ):
+    """ update starts with z=0 and runs z = z**2 + c
+        for a total of n times. It returns the final z.
+    """
+    z = 0
+    for x in range(n):
+        z = z**2 + c
+    return z
+        
+
+def inMSet( c, n ):
+    """ inMSet takes in
+            c for the update step of z = z**2+c
+            n, the maximum number of times to run that step
+        Then, it should return
+            False as soon as abs(z) gets larger than 2
+            True if abs(z) never gets larger than 2 (for n iterations)
+    """
+    z = 0
+    for x in range(n):
+        z = z**2 + c
+        if abs(z) > 2:
+            return False
+    return True
+
